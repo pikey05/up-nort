75b1d40c9398b0a2ddae799574582e2f7a363d90
Sean Cook <smcook55@gmail.com>
2023-10-31 15:58:57 -0500

    add images

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// docker run --rm -itd --name up-nort --workdir=/opt/app -v $(pwd):/opt/app -p 0.0.0.0:3000:3000 -e APP_ENV=DEV --entrypoint /bin/sh node:20.9.0-alpine3.18
+
 import React, { useState, useContext } from 'react';
 
 import About from './components/About/About';
--- src/components/UpNortImages/ImportImages.js
+++ src/components/UpNortImages/ImportImages.js
@@ -1,16 +1,55 @@
-import img01 from '../../assets/01.jpg'
-import img02 from '../../assets/02.jpg'
-import img03 from '../../assets/03.jpg'
-import img04 from '../../assets/04.jpg'
-import img05 from '../../assets/05.jpg'
-import img06 from '../../assets/06.jpg'
-import img07 from '../../assets/07.jpg'
-import img08 from '../../assets/08.jpg'
-import img09 from '../../assets/09.jpg'
-import img10 from '../../assets/10.jpg'
-import img11 from '../../assets/11.jpg'
-import img12 from '../../assets/12.jpg'
-import img13 from '../../assets/13.jpg'
+import img01 from '../../assets/1.jpg';
+import img02 from '../../assets/2.jpg';
+import img03 from '../../assets/3.jpg';
+import img04 from '../../assets/4.jpg';
+import img05 from '../../assets/5.jpg';
+import img06 from '../../assets/6.jpg';
+import img07 from '../../assets/7.jpg';
+import img08 from '../../assets/8.jpg';
+import img09 from '../../assets/9.jpg';
+import img10 from '../../assets/10.jpg';
+import img11 from '../../assets/11.jpg';
+import img12 from '../../assets/12.jpg';
+import img13 from '../../assets/13.jpg';
+import img14 from '../../assets/14.jpg';
+import img15 from '../../assets/15.jpg';
+import img16 from '../../assets/16.jpg';
+import img17 from '../../assets/17.jpg';
+import img18 from '../../assets/18.jpg';
+import img19 from '../../assets/19.jpg';
+import img20 from '../../assets/20.jpg';
+import img21 from '../../assets/21.jpg';
+import img22 from '../../assets/22.jpg';
+import img23 from '../../assets/23.jpg';
+import img24 from '../../assets/24.jpg';
+import img25 from '../../assets/25.jpg';
+import img26 from '../../assets/26.jpg';
+import img27 from '../../assets/27.jpg';
+import img28 from '../../assets/28.jpg';
+import img29 from '../../assets/29.jpg';
+import img30 from '../../assets/30.jpg';
+import img31 from '../../assets/31.jpg';
+import img32 from '../../assets/32.jpg';
+import img33 from '../../assets/33.jpg';
+import img34 from '../../assets/34.jpg';
+import img35 from '../../assets/35.jpg';
+import img36 from '../../assets/36.jpg';
+import img37 from '../../assets/37.jpg';
+import img38 from '../../assets/38.jpg';
+import img39 from '../../assets/39.jpg';
+import img40 from '../../assets/40.jpg';
+import img41 from '../../assets/41.jpg';
+import img42 from '../../assets/42.jpg';
+import img43 from '../../assets/43.jpg';
+import img44 from '../../assets/44.jpg';
+import img45 from '../../assets/45.jpg';
+import img46 from '../../assets/46.jpg';
+import img47 from '../../assets/47.jpg';
+import img48 from '../../assets/48.jpg';
+import img49 from '../../assets/49.jpg';
+import img50 from '../../assets/50.jpg';
+import img51 from '../../assets/51.jpg';
+import img52 from '../../assets/52.jpg';
 
 const imageList = [
   {
@@ -65,6 +104,162 @@ const imageList = [
     label: 'Image 13',
     imgPath: img13
   },
-]
+  {
+    label: 'Image 14',
+    imgPath: img14
+  },
+  {
+    label: 'Image 15',
+    imgPath: img15
+  },
+  {
+    label: 'Image 16',
+    imgPath: img16
+  },
+  {
+    label: 'Image 17',
+    imgPath: img17
+  },
+  {
+    label: 'Image 18',
+    imgPath: img18
+  },
+  {
+    label: 'Image 19',
+    imgPath: img19
+  },
+  {
+    label: 'Image 20',
+    imgPath: img20
+  },
+  {
+    label: 'Image 21',
+    imgPath: img21
+  },
+  {
+    label: 'Image 22',
+    imgPath: img22
+  },
+  {
+    label: 'Image 23',
+    imgPath: img23
+  },
+  {
+    label: 'Image 24',
+    imgPath: img24
+  },
+  {
+    label: 'Image 25',
+    imgPath: img25
+  },
+  {
+    label: 'Image 26',
+    imgPath: img26
+  },
+  {
+    label: 'Image 27',
+    imgPath: img27
+  },
+  {
+    label: 'Image 28',
+    imgPath: img28
+  },
+  {
+    label: 'Image 29',
+    imgPath: img29
+  },
+  {
+    label: 'Image 30',
+    imgPath: img30
+  },
+  {
+    label: 'Image 31',
+    imgPath: img31
+  },
+  {
+    label: 'Image 32',
+    imgPath: img32
+  },
+  {
+    label: 'Image 33',
+    imgPath: img33
+  },
+  {
+    label: 'Image 34',
+    imgPath: img34
+  },
+  {
+    label: 'Image 35',
+    imgPath: img25
+  },
+  {
+    label: 'Image 36',
+    imgPath: img26
+  },
+  {
+    label: 'Image 37',
+    imgPath: img37
+  },
+  {
+    label: 'Image 38',
+    imgPath: img38
+  },
+  {
+    label: 'Image 39',
+    imgPath: img39
+  },
+  {
+    label: 'Image 40',
+    imgPath: img40
+  },
+  {
+    label: 'Image 41',
+    imgPath: img41
+  },
+  {
+    label: 'Image 42',
+    imgPath: img42
+  },
+  {
+    label: 'Image 43',
+    imgPath: img43
+  },
+  {
+    label: 'Image 44',
+    imgPath: img44
+  },
+  {
+    label: 'Image 45',
+    imgPath: img45
+  },
+  {
+    label: 'Image 46',
+    imgPath: img46
+  },
+  {
+    label: 'Image 47',
+    imgPath: img47
+  },
+  {
+    label: 'Image 48',
+    imgPath: img48
+  },
+  {
+    label: 'Image 49',
+    imgPath: img49
+  },
+  {
+    label: 'Image 50',
+    imgPath: img50
+  },
+  {
+    label: 'Image 51',
+    imgPath: img51
+  },
+  {
+    label: 'Image 52',
+    imgPath: img52
+  }
+];
 
-export default imageList
+export default imageList;
